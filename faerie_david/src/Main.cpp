57e706f9394e0adc2f483a926a7edd22f1067baf
DelRey22 <rs0155@uah.edu>
2025-02-12 20:59:02 -0600

    added as7341 code from website

--- faerie_david/src/Main.cpp
+++ faerie_david/src/Main.cpp
@@ -11,18 +11,18 @@
 #include <Arduino.h>
 #include <SPI.h>    // Fixes compilation issue with Adafruit BusIO
 #include <ESP32Servo.h>  // For SCABBARD servo (unused)
-#include "AstraREVCAN.h"
+#include "AstraREVCAN.h"//remove for canastra in discord
 #include <cmath>  // for abs()
 
 #include "Adafruit_SHT31.h"  // adafruit/Adafruit SHT31 Library
 #ifndef REV_PWM
-#    include "AstraMotors.h"
+#include "AstraMotors.h"
 
 #endif
 #include "AstraMisc.h"
 
 #include "project/FAERIE.h"
-
+#include <Adafruit_AS7341.h> // adafruit/Adafruit AS7341 Library
 
 //-----------//
 // Constants //
@@ -42,6 +42,7 @@ Servo servo;
 // SHT 31 in SCABBARD
 Adafruit_SHT31 sht31 = Adafruit_SHT31();  // Faerie HUM/TEMP Sensor
 
+Adafruit_AS7341 as7341 = Adafruit_AS7341(); // Faerie Wavelenght sensor
 // Last millis value that sht temp and hum data was sent to socket
 uint32_t lastDataSend = 0;
 
@@ -103,7 +104,7 @@ uint32_t lastMotorCmd = 0;
 void stopREVMotor();
 void loopHeartbeats();
 String getSHTData(void);
-
+String getAS7341Data(void);
 
 //-------------//
 // Begin Setup //
@@ -558,6 +559,60 @@ String getSHTData(void) {
         res += "999.9";
 
     return res;
+}
+
+String getAS7341Data(void){
+
+      Serial.begin(115200);
+    
+      // Wait for communication with the host computer serial monitor
+      while (!Serial) {
+        delay(1);
+      }
+      
+      if (!as7341.begin()){
+        Serial.println("Could not find AS7341");
+        while (1) { delay(10); }
+      }
+    
+      as7341.setATIME(100);
+      as7341.setASTEP(999);
+      as7341.setGain(AS7341_GAIN_256X);
+    }
+    
+    
+      
+      if (!as7341.readAllChannels()){
+        Serial.println("Error reading all channels!");
+        return;
+      }
+    
+      // Print out the stored values for each channel
+      Serial.print("F1 415nm : ");
+      Serial.println(as7341.getChannel(AS7341_CHANNEL_415nm_F1));
+      Serial.print("F2 445nm : ");
+      Serial.println(as7341.getChannel(AS7341_CHANNEL_445nm_F2));
+      Serial.print("F3 480nm : ");
+      Serial.println(as7341.getChannel(AS7341_CHANNEL_480nm_F3));
+      Serial.print("F4 515nm : ");
+      Serial.println(as7341.getChannel(AS7341_CHANNEL_515nm_F4));
+      Serial.print("F5 555nm : ");
+      Serial.println(as7341.getChannel(AS7341_CHANNEL_555nm_F5));
+      Serial.print("F6 590nm : ");
+      Serial.println(as7341.getChannel(AS7341_CHANNEL_590nm_F6));
+      Serial.print("F7 630nm : ");
+      Serial.println(as7341.getChannel(AS7341_CHANNEL_630nm_F7));
+      Serial.print("F8 680nm : ");
+      Serial.println(as7341.getChannel(AS7341_CHANNEL_680nm_F8));
+    
+      Serial.print("Clear    : ");
+      Serial.println(as7341.getChannel(AS7341_CHANNEL_CLEAR));
+    
+      Serial.print("Near IR  : ");
+      Serial.println(as7341.getChannel(AS7341_CHANNEL_NIR));
+    
+      Serial.println("");
+    
 }
 
 void stopREVMotor() {
